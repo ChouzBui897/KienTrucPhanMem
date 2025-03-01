aba2ad8ea0185fdae50cc1274f648da7cc18b717
ChouzBui897 <167747888+ChouzBui897@users.noreply.github.com>
2025-03-01 14:12:51 +0700

    commit

--- ASC.Solution/ASC.Web/wwwroot/js/init.js
+++ ASC.Solution/ASC.Web/wwwroot/js/init.js
@@ -2,6 +2,5 @@
   $(function(){
     $('.sidenav').sidenav();
     $('.parallax').parallax();
-    $('.collapsible').collapsible(); // Khởi tạo collapsible ngay trong jQuery
   }); // end of document ready
 })(jQuery); // end of jQuery name space
